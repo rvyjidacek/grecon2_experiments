a195cca6f01687f58c99ab0895f96c8d34724302
Roman Vyjidacek <roman@vyjidacek.cz>
2022-02-03 12:48:03 +0100

    Update

--- Sources/grecon2_experiments/main.swift
+++ Sources/grecon2_experiments/main.swift
@@ -1,7 +1,9 @@
 import FcaKit
 import Foundation
 
-FileManager.default.changeCurrentDirectoryPath("/Users/romanvyjidacek/Library/Mobile Documents/com~apple~CloudDocs/UPOL/phd/research/grecon2_experiments/")
+#if DEBUG
+    FileManager.default.changeCurrentDirectoryPath("/Users/romanvyjidacek/Library/Mobile Documents/com~apple~CloudDocs/UPOL/phd/research/grecon2_experiments/")
+#endif
 
 do {
     try compareGreConAndGreConDCoverage()
