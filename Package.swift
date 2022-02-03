89bc14a4b92a28ce4763d5dc7b6676b4e4c8047d
Roman Vyjidacek <roman@vyjidacek.cz>
2022-02-03 13:00:58 +0100

    Update package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.1.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,10 +7,10 @@ let package = Package(
     name: "grecon2_experiments",
     platforms: [.macOS(.v10_12)],
     dependencies: [
-        .package(name: "FcaKit", url: "https://github.com/rvyjidacek/FcaKit.git",  from: "1.2.1"),
+        .package(url: "https://github.com/rvyjidacek/FcaKit.git",  from: "1.2.1"),
     ],
     targets: [
-        .executableTarget(
+        .target(
             name: "grecon2_experiments",
             dependencies: ["FcaKit"]),
         .testTarget(
