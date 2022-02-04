016bfc8749960b7544fa8853ef6c6e86bab954fa
Roman Vyjidacek <roman@vyjidacek.cz>
2022-02-04 08:10:40 +0100

    Update

--- Package.swift
+++ Package.swift
@@ -6,17 +6,16 @@ import PackageDescription
 let package = Package(
     name: "grecon2_experiments",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/rvyjidacek/FcaKit.git",  from: "1.2.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "grecon2_experiments",
-            dependencies: []),
+            dependencies: ["FcaKit"]),
         .testTarget(
             name: "grecon2_experimentsTests",
-            dependencies: ["grecon2_experiments"]),
+            dependencies: ["grecon2_experiments", "FcaKit"]),
     ]
 )
